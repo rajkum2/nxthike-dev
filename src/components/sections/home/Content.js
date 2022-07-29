75c9e3735807c2741c6b6b97d8150b8b84ff3ba9
Shubham Mondal <shubhamank002@gmail.com>
2022-07-29 15:10:53 +0530

    Add two sections

--- src/components/sections/home/Content.js
+++ src/components/sections/home/Content.js
@@ -2,52 +2,58 @@ import { useState } from "react";
 import Achievement from "./Achievements";
 import BannerSlider from "./BannerSlider";
 import Categories from "./Categories";
+import Testimonials from "./Testimonials";
+import JobSeekers from "./JobSeekers";
 import Featured from "./Featured";
 import Jobs from "./LatestJobs";
 import Offer from "./Offer";
 import SearchBar from "./SearchBar";
 import Info from "./SomeInfo";
+import "./home.css";
+
 const Content = () => {
-  const [visible, setVisible] = useState(false);
+    const [visible, setVisible] = useState(false);
 
-  const toggleVisible = () => {
-    const scrolled = document.documentElement.scrollTop;
-    if (scrolled > 100) {
-      setVisible(true);
-    } else if (scrolled <= 100) {
-      setVisible(false);
-    }
-  };
+    const toggleVisible = () => {
+        const scrolled = document.documentElement.scrollTop;
+        if (scrolled > 100) {
+            setVisible(true);
+        } else if (scrolled <= 100) {
+            setVisible(false);
+        }
+    };
 
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "auto",
-    });
-  };
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "auto",
+        });
+    };
 
-  window.addEventListener("scroll", toggleVisible);
-  return (
-    <>
-      <main className="body-section">
-        {/* <SearchBar /> */}
-        <BannerSlider />
-        <Achievement />
-        <Offer />
-        <Jobs />
-        <Categories />
-        {/* <Featured /> */}
-        <Info />
-      </main>
-      <button
-        onClick={scrollToTop}
-        id="pageup"
-        style={{ display: visible ? "block" : "none" }}
-      >
-        <i className="fas fa-arrow-up"></i>
-      </button>
-    </>
-  );
+    window.addEventListener("scroll", toggleVisible);
+    return (
+        <>
+            <main className="body-section">
+                {/* <SearchBar /> */}
+                <BannerSlider />
+                <Achievement />
+                <Offer />
+                <Jobs />
+                <Categories />
+                <Testimonials />
+                <JobSeekers />
+                {/* <Featured /> */}
+                <Info />
+            </main>
+            <button
+                onClick={scrollToTop}
+                id="pageup"
+                style={{ display: visible ? "block" : "none" }}
+            >
+                <i className="fas fa-arrow-up"></i>
+            </button>
+        </>
+    );
 };
 
 export default Content;
