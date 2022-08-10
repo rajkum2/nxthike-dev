457b9a4b9b9760a08bcfcfee5befbc9e1852fd11
Logicarts <kumar.alg2@gmail.com>
2022-08-10 19:24:24 +0530

    adding about us

--- src/components/sections/about/Content.js
+++ src/components/sections/about/Content.js
@@ -15,6 +15,7 @@ const Content = () => {
             <div className="about_des">
               <p>
                 Our vision is to have a single platform for Employers - making easier for hiring, and for Job Seekers & Freelancer - to earn more, across countries seemlesly and easily without any hassle.
+                We’re a Full-service recruiting, Executive recruiting, and consulting company. In particular, we focus on executive recruiting and human resources consulting. While our company is new, our team has tons of industry experience.
               </p>
               <p>
               Looking for Hiring Talent or Looking for a new Opportunity for your career, we got it covered!
@@ -41,12 +42,15 @@ const Content = () => {
                 <div className="choose_des">
                   <p>
                     With NxtHike, Hiring & getting Hired is easier now!
+                    At NxtHike, we have specialized in many main job areas, which we have full understanding in and which we have a wide pool of employers and talent looking to get connected. Whether looking for an opportunity or looking for talent in these areas, we will help you all steps of the way.<br/>
+                    Below are some of the Services we provide:
                   </p>
                   <ul>
                     <li>
                       <div className="abt142">
                         <i className="fas fa-check-circle"></i>
                         <p>
+                         <h5>Job Support:</h5>
                          Job Seekers can avail the support in job applications, resume preparation and acess to global companies.
                          
                         </p>
@@ -56,9 +60,17 @@ const Content = () => {
                       <div className="abt142">
                         <i className="fas fa-check-circle"></i>
                         <p>
-                          Cras rhoncus lorem lorem, a fermentum lacus congue
-                          vehicula. Nullam luct us mi eget nisl tincidunt
-                          lobortis.
+                        <h5>Multiple Job Types:</h5>
+                        We understand that you have unique staffing needs and/or looking for flexible job opportunities respectively and therefore, we have modeled our services for Full-time, Part-time, Freelancer, Onsite & Remote Jobs.
+                        </p>
+                      </div>
+                    </li>
+                    <li>
+                      <div className="abt142">
+                        <i className="fas fa-check-circle"></i>
+                        <p>
+                        <h5>HR Solutions:</h5>
+                        HR Solutions are integrated solutions that provide payroll, time and attendance, workers’ compensation, benefits, and risk mitigation. Delivered through an HR team and Professionals to help manage your compliance.
                         </p>
                       </div>
                     </li>
@@ -68,11 +80,7 @@ const Content = () => {
             </div>
             <div className="col-lg-6 col-md-12">
               <div className="about_video">
-                <iframe
-                  src="https://www.youtube.com/embed/TKnufs85hXk"
-                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                  allowfullscreen
-                ></iframe>
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/QkeT7MBr1nM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
               </div>
             </div>
           </div>
