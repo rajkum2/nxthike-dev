a1703bab8bb8206ca846d695c21ff64f7a734166
Shubham Mondal <shubhamank002@gmail.com>
2023-08-29 01:52:15 +0530

    number validation for us geolocation

--- src/components/layouts/MobileInputForm.js
+++ src/components/layouts/MobileInputForm.js
@@ -7,124 +7,117 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 
 export default function Mobileinputform({ Otp, clsmodal }) {
-  const [mobileNumber, setMobileNumber] = useState();
-  const [alertmsg, setAlertmsg] = useState({ status: false });
-  const [validation, setValidation] = useState(false);
-  const [flag, setFlag] = useState(true);
-  const [countryCode, setCountryCode] = useState("");
+    const [mobileNumber, setMobileNumber] = useState();
+    const [alertmsg, setAlertmsg] = useState({ status: false });
+    const [validation, setValidation] = useState(false);
+    const [flag, setFlag] = useState(true);
+    const [countryCode, setCountryCode] = useState("");
 
-  const getGeoInfo = () => {
-    axios
-      .get("https://ipapi.co/json/")
-      .then((response) => {
-        let data = response.data;
-        const text = data.country_code.toLowerCase();
-        setCountryCode(text);
-        // console.log(data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    console.log(countryCode);
-  };
+    const getGeoInfo = () => {
+        axios
+            .get("https://ipapi.co/json/")
+            .then((response) => {
+                let data = response.data;
+                const text = data.country_code.toLowerCase();
+                setCountryCode(text);
+                // console.log(data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        console.log(countryCode);
+    };
 
-  useEffect(() => {
-    getGeoInfo();
-  }, []);
+    useEffect(() => {
+        getGeoInfo();
+    }, []);
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-    //console.log("clicked");
-    if (!mobileNumber) {
-      setValidation(true);
-    } else if (mobileNumber.length !== 12) {
-      setValidation(true);
-    } else {
-      showMsg();
-      var phoneNumber = "+" + mobileNumber;
-      let recaptcha = new firebase.auth.RecaptchaVerifier("recaptcha", {
-        size: "invisible",
-        callback: (response) => {},
-      });
-      firebase
-        .auth()
-        .signInWithPhoneNumber(phoneNumber, recaptcha)
-        .then((confirmationResult) => {
-          console.log(confirmationResult);
-          otpvrid(confirmationResult);
-        })
-        .catch((error) => {
-          alert("Something went wrong please try again");
-          console.log(error);
-          clsmodal();
-        });
-    }
-  };
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if (!mobileNumber) {
+            setValidation(true);
+        } else if (countryCode === "in" && mobileNumber.length !== 12) {
+            setValidation(true);
+        } else if (countryCode === "us" && mobileNumber.length !== 11) {
+            setValidation(true);
+        } else {
+            showMsg();
+            var phoneNumber = "+" + mobileNumber;
+            let recaptcha = new firebase.auth.RecaptchaVerifier("recaptcha", {
+                size: "invisible",
+                callback: (response) => {},
+            });
+            firebase
+                .auth()
+                .signInWithPhoneNumber(phoneNumber, recaptcha)
+                .then((confirmationResult) => {
+                    console.log(confirmationResult);
+                    otpvrid(confirmationResult);
+                })
+                .catch((error) => {
+                    alert("Something went wrong please try again");
+                    console.log(error);
+                    clsmodal();
+                });
+        }
+    };
 
-  const otpvrid = (confirmationResult) => {
-    Otp(confirmationResult);
-  };
+    const otpvrid = (confirmationResult) => {
+        Otp(confirmationResult);
+    };
 
-  const showMsg = (e) => {
-    setAlertmsg({ status: true });
-  };
+    const showMsg = (e) => {
+        setAlertmsg({ status: true });
+    };
 
-  return (
-    <div className="phoneformdiv">
-      {alertmsg.status ? (
-        <h4 className="text-center text-success">Please Wait...</h4>
-      ) : (
-        <div>
-          <h4 className="text-center">Login / SignUp</h4>
-          <p className="modaltitle">Enter Your Phone Number:</p>
-          <form onSubmit={onSubmit}>
-            <div className="inputBox">
-              <PhoneInput
-                onlyCountries={["us", "in"]}
-                country={countryCode}
-                inputProps={{
-                  name: "phoneNumber",
-                  required: true,
-                  autoFocus: true,
-                }}
-                inputStyle={{
-                  width: "100%",
-                  height: "42px",
-                  fontSize: "18px",
-                }}
-                value={mobileNumber}
-                onChange={(phone) => setMobileNumber(phone)}
-              />
-            </div>
-            {validation && (
-              <p className="text-danger">Enter Valid Phone Number</p>
+    return (
+        <div className="phoneformdiv">
+            {alertmsg.status ? (
+                <h4 className="text-center text-success">Please Wait...</h4>
+            ) : (
+                <div>
+                    <h4 className="text-center">Login / SignUp</h4>
+                    <p className="modaltitle">Enter Your Phone Number:</p>
+                    <form onSubmit={onSubmit}>
+                        <div className="inputBox">
+                            <PhoneInput
+                                onlyCountries={["us", "in"]}
+                                country={countryCode}
+                                inputProps={{
+                                    name: "phoneNumber",
+                                    required: true,
+                                    autoFocus: true,
+                                }}
+                                inputStyle={{
+                                    width: "100%",
+                                    height: "42px",
+                                    fontSize: "18px",
+                                }}
+                                value={mobileNumber}
+                                onChange={(phone) => setMobileNumber(phone)}
+                            />
+                        </div>
+                        {validation && <p className="text-danger">Enter Valid Phone Number</p>}
+                        <div style={{ paddingBlock: "12px" }}>
+                            <button type="submit" className="modalbtn" name="submit">
+                                Login
+                            </button>
+                        </div>
+                        <div className="modalbox-tc">
+                            <input type="checkbox" checked={flag} onChange={() => setFlag(!flag)} />
+                            <label>
+                                {" "}
+                                I here by agree to the{" "}
+                                <Link to="/" target="_blank">
+                                    Terms and Conditions
+                                </Link>
+                            </label>
+                        </div>
+                        {!flag && <p className="text-danger">It should be accepted to login.</p>}
+                    </form>
+                </div>
             )}
-            <div style={{ paddingBlock: "12px" }}>
-              <button type="submit" className="modalbtn" name="submit">
-                Login
-              </button>
-            </div>
-            <div className="modalbox-tc">
-              <input
-                type="checkbox"
-                checked={flag}
-                onChange={() => setFlag(!flag)}
-              />
-              <label>
-                {" "}
-                I here by agree to the{" "}
-                <Link to="/" target="_blank">
-                  Terms and Conditions
-                </Link>
-              </label>
-            </div>
-            {!flag && (
-              <p className="text-danger">It should be accepted to login.</p>
-            )}
-          </form>
+            <div id="recaptcha"></div>
         </div>
-      )}
-      <div id="recaptcha"></div>
-    </div>
-  );
+    );
 }
