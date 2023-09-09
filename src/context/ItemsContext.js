ff3a300f4c49d0a2598fcdcd033c828761051f41
sergiomano1624 <124707005+sergiomano1624@users.noreply.github.com>
2023-09-10 00:30:56 +0530

    added latest and related jobs

--- src/context/ItemsContext.js
+++ src/context/ItemsContext.js
@@ -9,6 +9,7 @@ export default function ItemContext({ children }) {
     const [items, setItems] = useState([]);
     const [itemscount, setItemscount] = useState(0);
     const [cat, setCat] = useState("");
+    const [subCat, setSubCat] = useState("");
     const [loc, setLoc] = useState("");
     const [exp, setExp] = useState("");
     const [jobType, setJobType] = useState("");
@@ -75,6 +76,28 @@ export default function ItemContext({ children }) {
         }
     };
 
+    const fetchJobsByCategories = async (subCatId) => {
+        setError(false);
+        setLoading(true);
+        try {
+            let url;
+            let response;
+            if(subCatId !== ''){
+                const postData = {
+                    app_list_id: "app_3bc06fa714c48378fe253c0e59913b7d",
+                    status: 1,
+                    sub_cat_id: subCatId,
+                };
+                url = `${process.env.REACT_APP_API_URL}items/search/api_key/${process.env.REACT_APP_API_SECURITY_KEY}/limit/9/offset/${offset}`;
+                response = await axios.post(url, postData);
+            }
+            const data = response.data;
+            updateItemsState(data);
+        } catch (err) {
+            callError(err);
+        }
+    };
+
     const callError = (err) => {
         setLoading(false);
         setError(true);
@@ -92,6 +115,10 @@ export default function ItemContext({ children }) {
     const changeCat = (val) => {
         setItems([]);
         setCat(val);
+    };
+    const changeSubCat = (val) => {
+        setItems([]);
+        setSubCat(val);
     };
     const changeExp = (val) => {
         setItems([]);
@@ -175,6 +202,8 @@ export default function ItemContext({ children }) {
                 setItemscount,
                 cat,
                 setCat,
+                subCat,
+                setSubCat,
                 loc,
                 setLoc,
                 exp,
@@ -200,8 +229,10 @@ export default function ItemContext({ children }) {
                 offset,
                 setOffset,
                 fetchJobs,
+                fetchJobsByCategories,
                 updateItemsState,
                 changeCat,
+                changeSubCat,
                 clearCat,
                 changeExp,
                 clearExp,
